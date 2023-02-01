dc616cb88b71fe3375384eb52deaab5fc0643b87
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-02-01 12:58:09 -0500

    exercise one

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your first name?");
+            string? name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string? color = Console.ReadLine();
+            Console.WriteLine("what is your favorite animal");
+            string? animal = Console.ReadLine(); 
+            Console.WriteLine("What is your favorite band?");
+            string? band = Console.ReadLine();
+            Console.WriteLine("Here is your profile");
+            Console.WriteLine("********************");
+          
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
+
+
+
+
+
+
         }
     }
 }
